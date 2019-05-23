0de0be3294aba65d2d20620ede57d966994d57b7
jiavg <lylgjiang@163.com>
2019-05-23 16:03:35 +0800

    环境搭建

---
+++ Atcrowdfunding-manager-api/src/main/java/club/lylgjiang/manager/service/TestService.java
@@ -0,0 +1,10 @@
+package club.lylgjiang.manager.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface TestService {
+	
+	int insertUser(String name);
+	
+}
---
+++ Atcrowdfunding-manager-impl/src/main/java/club/lylgjiang/manager/dao/TestDao.java
@@ -0,0 +1,10 @@
+package club.lylgjiang.manager.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TestDao {
+	
+	int insert(String name);
+	
+}
